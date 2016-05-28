d1d7a41bcbae2200f63325335039f674613ec299
Bartosz Jarecki <Bartosz Jarecki>
2016-05-28 12:19:19 +0200

    Wyświetlanie znaków na tablicy graficznej w GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,32 @@
 import pygame, sys, os
 
+#Tablice danych przedstawiające plansze Sudoku i jeje stan;
+mainBoard = []#aktualne wartości prezentowane na planszy sudoku;
+for i in range(9):
+    mainBoard.append('100010001')
+constBoard = mainBoard#przechowuje miejsca w tablicy których nie można zmienić;
+
 #Funkcje wykorzystywane przez program;
+def lineSize(index):#Zwraca ile grubości lini doliczyć;
+    if index//3 == 1: return 4
+    if index//3 == 2: return 8
+    return 0
+
+def getChar(char):
+    myFont = pygame.font.SysFont('monospace', 25)
+    return myFont.render(char, 1, (0, 0, 0))
+
+def blitSudokuBoard(window, tab):
+    window.blit(gSudokuBoard, (150, 100))
+    for i in range(len(tab)):
+        for j in range(len(tab[i])):
+            x = 164 + lineSize(j) + 32 * j
+            y = 108 + lineSize(i) + 32 * i
+            if tab[i][j] == '0':
+                window.blit(getChar(' '), (x, y))
+            else:
+                window.blit(getChar(tab[i][j]), (x, y))
+
 def menuPanelEvent(mPos):
     menuPanel(mPos)
     gamePanel(mPos)
@@ -84,7 +110,7 @@ while True:
 
 #Rysowanie okna i jego elementów;
     window.fill(bgColour)
-    window.blit(gSudokuBoard, (150, 100))
+    blitSudokuBoard(window, mainBoard)
     window.blit(gMenuPanel, (0, 0))
     window.blit(gGamePanel, (520, 100))
     if isSelectedOption == True:
